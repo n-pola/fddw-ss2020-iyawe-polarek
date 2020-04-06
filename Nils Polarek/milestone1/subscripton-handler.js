8d817f0ffbd850401b245edbf490eab36f4f7a94
n-pola <n.polarek@live.de>
2020-04-06 16:40:17 +0200

    add Request/Reply Pattern for user app subscripton request

---
+++ Nils Polarek/milestone1/subscripton-handler.js
@@ -0,0 +1,52 @@
+var amqp = require("amqplib/callback_api");
+
+amqp.connect(
+  "amqp://dtnuecqi:gGpHnyj_8HKgJC_w2okKeZZJmXxkEnsn@bee.rmq.cloudamqp.com/dtnuecqi",
+  function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var exchange = "subscription";
+      var queue = "rpc_queue";
+
+      channel.assertExchange(exchange, "topic", {
+        durable: false,
+      });
+
+      channel.assertQueue(
+        "",
+        {
+          exclusive: true,
+        },
+        function (error2, q) {
+          if (error2) throw error2;
+
+          channel.bindQueue(q.queue, exchange, "sub.req");
+          channel.prefetch(1);
+
+          channel.consume(
+            q.queue,
+            function (msg) {
+              let id = 1;
+              console.log(msg);
+              channel.sendToQueue(
+                msg.properties.replyTo,
+                Buffer.from(id.toString()),
+                {
+                  correlationId: msg.properties.correlationId,
+                }
+              );
+
+              //channel.ack(msg);
+            },
+            { noAck: true }
+          );
+        }
+      );
+    });
+  }
+);
---
+++ Nils Polarek/milestone1/user-app.js
@@ -0,0 +1,104 @@
+var amqp = require("amqplib/callback_api");
+const readline = require("readline");
+
+var args = process.argv.slice(2);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const getLine = (function (msg) {
+  console.log(msg);
+  const getLineGen = (async function* () {
+    for await (const line of rl) {
+      yield line;
+    }
+  })();
+  return async () => (await getLineGen.next()).value;
+})();
+
+if (args[0] == null) {
+  init();
+} else {
+  listenTo(args[0]);
+}
+
+async function init() {
+  console.log("Start:");
+  let start = await getLine();
+  console.log("Destination:");
+  let destination = await getLine();
+  let id = null;
+  console.log({ start, destination });
+
+  amqp.connect(
+    "amqp://dtnuecqi:gGpHnyj_8HKgJC_w2okKeZZJmXxkEnsn@bee.rmq.cloudamqp.com/dtnuecqi",
+    function (error0, connection) {
+      if (error0) {
+        throw error0;
+      }
+      connection.createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+        channel.assertQueue(
+          "",
+          {
+            exclusive: true,
+          },
+          function (error2, q) {
+            if (error2) {
+              throw error2;
+            }
+            var correlationId = generateUuid();
+            var msg = { start: start, destination: destination };
+            msg = JSON.stringify(msg);
+
+            console.log(" [x] Requesting ID");
+
+            channel.assertExchange("subscription", "topic", {
+              durable: false,
+            });
+
+            channel.consume(
+              q.queue,
+              function (msg) {
+                if (msg.properties.correlationId == correlationId) {
+                  console.log(
+                    " [.] Listening to ID %s",
+                    msg.content.toString()
+                  );
+                  listenTo(parseInt(msg.content));
+                  setTimeout(function () {
+                    connection.close();
+                  }, 500);
+                }
+              },
+              {
+                noAck: true,
+              }
+            );
+
+            channel.publish("subscription", "sub.req", Buffer.from(msg), {
+              correlationId: correlationId,
+              replyTo: q.queue,
+            });
+          }
+        );
+      });
+    }
+  );
+}
+
+function listenTo(id) {
+  console.log(id);
+}
+
+function generateUuid() {
+  return (
+    Math.random().toString() +
+    Math.random().toString() +
+    Math.random().toString()
+  );
+}

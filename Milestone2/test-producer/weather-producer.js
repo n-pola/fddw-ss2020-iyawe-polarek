2b1cf62ac4994cb397380c920c544e0d9e8f72e2
n-pola <n.polarek@live.de>
2020-05-01 11:51:28 +0200

    add test producer

---
+++ Milestone2/test-producer/weather-producer.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+var amqp = require("amqplib/callback_api");
+
+amqp.connect(process.env.AMQP_URL, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    let exchange = "fddw";
+    let topic = "weather.add";
+    var msg = { ID: 123, destination: "Sölden" };
+    msg = JSON.stringify(msg);
+
+    channel.publish(exchange, topic, Buffer.from(msg));
+  });
+});

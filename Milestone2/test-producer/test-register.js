9eab68eaa6527cf50a5878b77265f28a7ae03272
Nils <56259168+n-pola@users.noreply.github.com>
2020-05-02 21:47:57 +0200

    update Weather Handling to new POC (#9)
    * update gitignore
    
    * change registation handler to match POC
    
    * implement answer to anonymous queue in handler + test
    
    * change name of weather service to weather fetch
    
    * add Weather Handler
    
    * update weather Fetch to POC
    
    * update DB function for weather to work for new handling
    
    * update sheduler to POC
    
    * changed to Berlin because "Soelden" wasnt found

--- Milestone2/test-producer/test-register.js
+++ Milestone2/test-producer/test-register.js
@@ -1,24 +1,49 @@
-var doetenv = require('dotenv').config()
-var amqp = require('amqplib/callback_api')
+var doetenv = require("dotenv").config();
+var amqp = require("amqplib/callback_api");
 
 amqp.connect(process.env.AMQP_URL, (error0, conntection) => {
-    if (error0) {
-        console.error(error0.message);
+  if (error0) {
+    console.error(error0.message);
+  }
+  conntection.createChannel((error1, channel) => {
+    if (error1) {
+      console.error(error1.message);
     }
-    conntection.createChannel((error1, channel) => {
-        if (error1) {
-            console.error(error1.message)
-        }
 
+    var queue = "register_queue";
+    var message = {
+      start_date: "2020.05.01",
+      end_date: "2020.05.04",
+      destination: "Cologne"
+    };
 
-        var queue = "register_queue"
-        var message = { "start_date": "2020.05.01", "end_date": "2020.05.04", "destination": "Soelden" }
+    channel.assertQueue(
+      "",
+      {
+        exclusive: true
+      },
+      function (error2, q) {
+        if (error2) throw error2;
 
-        channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)))
-        console.log(`sent ${JSON.stringify(message)} to ${queue}`)
+        var msg = JSON.stringify(message);
 
-    })
-}
+        channel.consume(
+          q.queue,
+          function (msg) {
+            console.log(msg.content.toString());
+            channel.ack(msg);
+            channel.deleteQueue(q.queue);
+            channel.close();
+          },
+          {
+            noAck: false
+          }
+        );
 
-)
-
+        channel.publish("fddw-endpoint", "group.create", Buffer.from(msg), {
+          replyTo: q.queue
+        });
+      }
+    );
+  });
+});

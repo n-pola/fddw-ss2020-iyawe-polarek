88ee1f58aaeb7854d8e60dd97492911e5dc4e741
cd-Roid <giyawe98@gmail.com>
2020-05-06 11:46:30 +0200

    formatted response to show individual car topics depending on the number of cars in a trip.

--- Milestone2/info-provider.js
+++ Milestone2/info-provider.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const amqp = require("amqplib/callback_api");
 const weatherFormatter = require("./lib/weatherFormatter")
 const carCounter = require("./lib/carCounter")
+const topicLister = require("./lib/carTopicLister")
 const MongoClient = require("mongodb").MongoClient;
 var dbo;
 
@@ -12,52 +13,44 @@ MongoClient.connect(process.env.MONGO_BASE, { useUnifiedTopology: true },
         console.log("Connected");
         getInfo();
     })
-
 async function getInfo() {
     amqp.connect(process.env.AMQP_URL, function (error0, connection) {
         if (error0) {
             throw error0;
         }
-
         connection.createChannel((error1, channel) => {
             if (error1) throw error1;
 
             let queue = "info-queue"
             let exhange = "fddw-endpoint"
 
-
+            channel.assertQueue(queue, { durable: true })
             channel.bindQueue(queue, exhange, "*.info")
 
             channel.consume(queue, (msg) => {
                 var msgJSON = JSON.parse(msg.content)
-                console.log(msgJSON)
                 let id = msgJSON.id
-                console.log(id)
                 let chatID = msgJSON.adress
-                console.log(chatID)
-
 
                 dbo.collection("travels").findOne({ id: id }, (err, res) => {
                     if (err) throw err;
-
-                    var msgJSONa = ""
+                    var foramtedMsg = ""
 
                     if (res != null) {
-                        msgJSONa = `Trip to ${res.destination} from the ${res.startDate} to ${res.endDate}.\n
+                        foramtedMsg = `Trip to ${res.destination} from the ${res.startDate} to ${res.endDate}.\n
                         Weather forecast:\n ${weatherFormatter(res)} \n
                         Traffic: \n ${carCounter(res)} \n
-                        Possible Topic for this Group: \n all, weather, traffic, traffic.1, traffic.2.
+                        Possible Topic for this Group: \n all \n weather \n ${topicLister(res)}
                         Have a nice trip!🛬
                         `
-                        channel.sendToQueue(msg.properties.replyTo, Buffer.from(msgJSONa))
-
+                        channel.sendToQueue(msg.properties.replyTo, Buffer.from(foramtedMsg))
                     }
                     else {
-                        msgJSONa = "Seems like this group doesn't exsist..🤔"
-                        channel.sendToQueue(msg.properties.replyTo, Buffer.from(msgJSONa))
+                        foramtedMsg = "Seems like this group doesn't exsist..🤔"
+                        channel.sendToQueue(msg.properties.replyTo, Buffer.from(foramtedMsg))
                     }
+                    channel.ack(msg)
                 })
-                channel.ack(msg)
             }, {
                 noAck: false
             })
